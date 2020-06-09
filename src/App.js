eadaba99028d5d3bbc2b170e93f97d2b8d961b3c
Shahzebansari213 <66177653+Shahzebansari213@users.noreply.github.com>
2020-06-09 03:52:13 +0300

    Assignment Class 3 PIAIC

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import Hello from './Hello';
 import './App.css';
+import './Hello.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          SHahzeb Imran Ansari
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({name , age}) {
+  return <div> 
+   <strong>Hello Shahzeb Ansari </strong>
+   <h1 className= "myname"> Electrical Engineer</h1>
+   <ol> 
+     <li> Matric done in 2007</li>
+     <li>FSc done in 2009</li>
+     <li>Bachelors graduation in 2013</li>
+   </ol>
+   <br/><br/>
+   <h2 className= "myrealname">Interests</h2>
+   <ul>
+     <li>Book Reading</li>
+     <li>Programmung</li>
+   </ul>
+  </div>
+    
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import './Hello.css'
+
+function Hello({firstname}) {
+  return <p className= "myname"> Hello World and {firstname} </p>
+
+}
+
+export default Hello;
